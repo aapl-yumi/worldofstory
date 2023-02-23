056de68af46ebe7d92803ce0b3914a1d77138c4c
Yumi Izumi <me@yumiizumi.com>
2023-02-23 18:14:10 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://worldofstory.worldroad.org/",
+  site: "https://story.worldroad.org/",
   integrations: [tailwind(), partytown(), sitemap(), preact(), react()],
 });
